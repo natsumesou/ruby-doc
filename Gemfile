df9a5b7289150592fb9ac403a3f0c083f4587a6c
natsumesou <natsumesouxx@gmail.com>
2013-05-18 19:30:22 +0900

    default class

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'rdoc'
+gem 'rdtool'
+gem 'yard'
+gem 'sdoc'
+gem 'tomdoc'
---
+++ person.rb
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class Person
+
+  attr_reader :first_name, :last_name, :gender, :age
+
+  def initialize first_name, last_name, gender = nil,age = nil
+    @first_name = first_name
+    @last_name = last_name
+    @gender = gender
+    @age = age
+  end
+
+  def man?
+    @gender == :man
+  end
+
+  def woman?
+    @gender == :woman
+  end
+end
