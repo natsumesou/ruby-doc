83f7a0eae94dc0e937be7d1cf973f0aeff89b809
natsumesou <natsumesouxx@gmail.com>
2013-05-18 20:49:23 +0900

    yardoc用のドキュメント

--- person.rb
+++ person.rb
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 
+# 人間を表すクラス
 class Person
+  # @return [String] 名前
+  attr_reader :first_name
+  # @return [String] 苗字
+  attr_reader :last_name
+  # @return [Symbol] 性別
+  attr_reader :gender
+  # @return [Fixnum] 年齢
+  attr_reader :age
 
-  attr_reader :first_name, :last_name, :gender, :age
-
+  # @param first_name [String] 名前
+  # @param last_name [String] 苗字
+  # @param gender [Symbol] 性別
+  # @param age [Integer] 年齢
+  #
+  # @example
+  #  person = Person.new('ひろゆき', '松本', :man, 17)
+  #
+  # @return [Object] インスタンス
   def initialize first_name, last_name, gender = nil, age = nil
     @first_name = first_name
     @last_name = last_name
@@ -11,14 +27,22 @@ class Person
     @age = age
   end
 
+  # 男性かどうかを判定する
+  # @return [Boolean]
   def man?
     @gender == :man
   end
 
+  # 男性かどうかを判定する
+  # @deprecated
+  # @todo 消す
+  # @return [Boolean]
   def is_man?
     @gender == :man
   end
 
+  # 女性かどうかを判定する
+  # @see #man?
   def woman?
     @gender == :woman
   end
